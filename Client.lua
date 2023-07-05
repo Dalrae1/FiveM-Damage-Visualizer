85a0b6e0e37e8f5472c9a769f5989180ac01332e
Dyrek <68273911+DyrekKing@users.noreply.github.com>
2023-07-05 02:13:56 +0200

    Rear-body, 3 stage damage detection (#2)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Fixed an IF statement @ 154

--- Client.lua
+++ Client.lua
@@ -134,15 +134,28 @@ local headlightFunctions = {
     [true]=GetIsLeftVehicleHeadlightDamaged,
     [false]=GetIsRightVehicleHeadlightDamaged
 }
+
+local taillightBoneNames = {
+    [true]="taillight_l",
+    [false]="taillight_r"
+}
+
 function GetBodyState(vehicle,front,leftSide)
     local isBouncing = IsVehicleBumperBouncing(vehicle,front)
     local isBroken = IsVehicleBumperBrokenOff(vehicle,front) and isBouncing
+
     if front then 
         local headlightBroken = headlightFunctions[leftSide](vehicle)
         if headlightBroken and not isBroken and not isBouncing then 
             return 1
         end
+    elseif not front then 
+        local tailLight = GetEntityBoneIndexByName(vehicle,taillightBoneNames[leftSide])
+        if GetEntityBoneRotation(vehicle,tailLight) == vector3(0,0,0) and not isBroken and not isBouncing then 
+            return 1 
+        end
     end 
+
     if isBroken then
         return 3
     elseif isBouncing then
